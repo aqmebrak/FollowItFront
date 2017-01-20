42e5005d0d28ef6f0397c85416633fc1c30a4abe
Jeremy Melchor <melchor.pro@gmail.com>
2017-01-20 11:20:09 +0100

    2D picker sur wearable

--- wear/src/main/java/polytech/followit/MainActivity.java
+++ wear/src/main/java/polytech/followit/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.GridViewPager;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -31,20 +31,16 @@ public class MainActivity extends Activity implements
 
     private TextView mTextView;
     private GoogleApiClient googleClient;
+    private GridViewPager pager;
 
-    private ArrayList<String> instructions;
+    public static ArrayList<String> instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        pager = (GridViewPager) findViewById(R.id.grid_view_pager);
 
         // Build a new GoogleApiClient for the Wearable API
         googleClient = new GoogleApiClient.Builder(this)
@@ -96,7 +92,7 @@ public class MainActivity extends Activity implements
                     DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
                     Log.d(TAG, "RECU" + dataMap.getStringArrayList("instructions"));
                     instructions = dataMap.getStringArrayList("instructions");
-                    mTextView.setText(instructions.toString());
+                    pager.setAdapter(new GridPagerAdapter(this,getFragmentManager()));
                 }
             } else if (event.getType() == DataEvent.TYPE_DELETED) {
                 // DataItem deleted
